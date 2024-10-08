1e0f76ad452524c4faa2839fc397e0e94cd4885c
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2024-10-07 20:52:17 -0400

    show upcoming events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+// src/App.js
+
+import EventList from './components/EventList';
+
 import './App.css';
 
 const App = () => {
  return (
    <div className="App">
+      <div id="event-list"></div>
    </div>
  );
 }
 
 export default App;
+
 
---
+++ src/components/EventList.js
@@ -0,0 +1,11 @@
+// src/components/EventList.js
+
+const EventList = () => {
+    return (
+      <ul id="event-list" role="list" aria-label="event-list">
+      </ul>
+    );
+  }
+  
+  export default EventList;
+  
