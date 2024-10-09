b7988d034b30369df292a619bb97e42c563ad1c0
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2024-10-09 12:44:26 -0400

    code = token 1x no cors issue

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -61,9 +61,7 @@ module.exports.getAccessToken = async (event) => {
         statusCode: 200,
         headers: {
           'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
-          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          'Access-Control-Allow-Credentials': true
         },
         body: JSON.stringify(results),
       };
