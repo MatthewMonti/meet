d74eaac9cee4d3bcf26a216e889cecbe1ad778be
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2024-10-12 13:42:40 -0400

    list of suggestions show + hide tests

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,16 +1,22 @@
 // src/components/CitySearch.js
 
+import { useState } from "react";
+
 const CitySearch = () => {
-    return (
-      <div id="city-search">
-        <input
-          type="text"
-          className="city"
-                  placeholder="Search for a city"
-        />
-      </div>
-    )
-   }
-  
-  export default CitySearch;
-  
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  return (
+    <div id="city-search">
+      <input
+        type="text"
+        className="city"
+                placeholder="Search for a city"
+        onFocus={() => setShowSuggestions(true)}
+      />
+      {showSuggestions ? <ul className="suggestions"></ul> : null}
+    </div>
+  )
+}
+
+export default CitySearch;
+
+
