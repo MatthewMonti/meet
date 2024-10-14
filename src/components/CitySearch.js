a4689521930d70d350ea42cc8eeb9550dcff9894
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2024-10-13 22:05:47 -0400

    Scenario 3: User Can Select a City from the Suggested List

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -15,21 +15,27 @@ const CitySearch = ({ allLocations }) => {
     setQuery(value);
     setSuggestions(filteredLocations);
   };
+  const handleItemClicked = (event) => {
+    const value = event.target.textContent;
+    setQuery(value);
+    setShowSuggestions(false); // to hide the list
+  };
+
 
   return (
     <div id="city-search">
       <input
         type="text"
         className="city"
-                placeholder="Search for a city"
+        placeholder="Search for a city"
         value={query}
         onFocus={() => setShowSuggestions(true)}
         onChange={handleInputChanged}
       />
-            {showSuggestions ?
+      {showSuggestions ?
         <ul className="suggestions">
           {suggestions.map((suggestion) => {
-            return <li key={suggestion}>{suggestion}</li>
+            return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
           })}
           <li key='See all cities'>
             <b>See all cities</b>
