dab3daa9dcc82b3ac21809f1a6e0f59755db1a45
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2024-10-21 16:19:15 -0400

    reset

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import CitySearch from './components/CitySearch';
+import EventList from './components/EventList';
+import './App.css';
+
+const App = () => {
+ return (
+   <div className="App">
+     <CitySearch />
+     <EventList />
+   </div>
+ );
+}
+
+export default App;
