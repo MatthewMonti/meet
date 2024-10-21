3ac667f6256516839c4e19fe76816e2192410ab7
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2024-10-21 17:12:27 -0400

    Feature 1 WORKING IN FULL dublicate files cleaned and removed. = Sorry

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -2,7 +2,7 @@
 
 import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
-import {getEvents} from '../api';
+import { getEvents } from '../api';
 describe('<EventList /> component', () => {
    let EventListComponent;
  beforeEach(() => {
@@ -12,9 +12,11 @@ describe('<EventList /> component', () => {
   test('has an element with "list" role', () => {
     expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
   });
+
   test('renders correct number of events', async () => {
     const allEvents = await getEvents(); 
     EventListComponent.rerender(<EventList events={allEvents} />);
-      expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
   });
+
 });
