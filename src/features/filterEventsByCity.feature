687ff844464570737de112d8583386f06ad6e7e7
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2024-11-27 14:05:42 -0500

    scenario 1 plus match test description

--- src/features/filterEventsByCity.feature
+++ src/features/filterEventsByCity.feature
@@ -1,6 +1,6 @@
 Feature: Filter events by city
  Scenario: When user hasn’t searched for a city, show upcoming events from all cities.
-  Given user hasn’t searched for any city
+  Given User hasn’t searched for any city
   When the user opens the app
   Then the user should see the list of all upcoming events.
 
@@ -8,6 +8,7 @@ Feature: Filter events by city
   Given the main page is open
   When user starts typing in the city textbox
   Then the user should receive a list of cities (suggestions) that match what they’ve typed
+ 
  Scenario: User can select a city from the suggested list.
   Given user was typing “Berlin” in the city textbox
   And the list of suggested cities is showing
--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,56 +1,66 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
-import {render} from '@testing-library/react';
+import { act } from 'react';
+import {render, within, waitFor, screen } from '@testing-library/react';
 import App from '../App';
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
 defineFeature(feature, test => {
-    test('when user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
+    test('When user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
       given('user hasn’t searched for any city', () => {
   
       });
   
-      when('the user opens the app', () => {
-  
+      let AppComponent;
+      when('the user opens the app', async () => {
+        await act(async () => {
+        AppComponent = render(<App />);
+        });
       });
   
-      then('the user should see the list of upcoming events.', () => {
-  
-      });
+    then('the user should see the list of all upcoming events.', async () => {
+       const AppDOM = AppComponent.container.firstChild;
+       const EventListDOM= screen.queryByTestId('event-list');  
+
+       await waitFor(() => {
+         const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+         expect(EventListItems.length).toBe(32);
+        });
+       });
     });
   
-    test('user should see a list of suggestions when they search for a city', ({ given, when, then }) => {
+    test('User should see a list of suggestions when they search for a city.', ({ given, when, then }) => {
       given('the main page is open', () => {
   
       });
   
-      when('user starts typing in the city textbox', () => {
+      when('user starts typing in the city textbox', async () => {
   
       });
   
-      then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
+      then('the user should receive a list of cities (suggestions) that match what they’ve typed', async () => {
   
       });
     });
   
   
-    test('user can select a city from the suggested list', ({ given, and, when, then }) => {
-      given('user was typing “Berlin” in the city textbox', () => {
+    test('User can select a city from the suggested list.', ({ given, and, when, then }) => {
+      given('user was typing “Berlin” in the city textbox', async () => {
   
       });
   
-      and('the list of suggested cities is showing', () => {
+      and('the list of suggested cities is showing', async () => {
   
       });
   
-      when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
+      when('the user selects a city (e.g., “Berlin, Germany”) from the list', async () => {
   
       });
   
-      then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
+      then('their city should be changed to that city (i.e., “Berlin, Germany”)', async () => {
   
       });
   
-      and('the user should receive a list of upcoming events in that city', () => {
+      and('the user should receive a list of upcoming events in that city', async () => {
   
       });
     });
