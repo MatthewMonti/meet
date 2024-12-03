5a05840a23ad341fd5079636ba29a552879306cc
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2024-12-03 14:25:57 -0500

    setup steps for feature 3

--- src/features/listCount.feature
+++ src/features/listCount.feature
@@ -1,6 +1,11 @@
-Feature: Display Events in EventList Component
+Feature: Specificy Number of Events
 
-  Scenario: Display the correct number of events
-    Given the EventList component is mounted
-    When the page is loaded
-    Then the EventList should display exactly 35 events
+  Scenario: When user has not specified a number, 32 events are shown by default.
+    Given user on main page 
+    When number in event count textbox default set to 32 
+    Then the number of events displayed is exactly 35 events
+
+  Scenario: User can change the number of events displayed
+    Given clicks on texbox with event number count
+    When user changes the number of events
+    Then page displays number of events user prefers displayed
--- src/features/listCount.test.js
+++ src/features/listCount.test.js
@@ -3,16 +3,30 @@ import { loadFeature, defineFeature } from 'jest-cucumber';
 const feature = loadFeature('./src/features/listCount.feature');
 
 defineFeature(feature, test => {
-    test('Display the correct number of events', ({ given, when, then }) => {
-        given('the EventList component is mounted', () => {
+    test('When user has not specified a number, 32 events are shown by default.', ({ given, when, then }) => {
+        given('user on main page', () => {
 
         });
 
-        when('the page is loaded', () => {
+        when(/^number in event count textbox default set to (\d+)$/, (arg0) => {
 
         });
 
-        then('the EventList should display exactly "35" events', () => {
+       then(/^the number of events displayed is exactly (\d+) events$/, (arg0) => {
+
+        });
+    });
+
+    test('User can change the number of events displayed', ({ given, when, then }) => {
+        given('clicks on texbox with event number count', () => {
+
+        });
+
+        when('user changes the number of events', () => {
+
+        });
+
+        then('page displays number of events user prefers displayed', () => {
 
         });
     });
