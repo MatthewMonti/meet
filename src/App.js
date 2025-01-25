7690787810a78dbf142910dfc1e802ed7a095448
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2025-01-25 12:36:15 -0500

    style upgrades & app home logo

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Meetup App</h1>
+      <h1>Meet App</h1>
       <img className="time" alt="meet-logo"src={calendar}></img>
       <div className="cityError-Message">
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
