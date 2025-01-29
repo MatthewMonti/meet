0aefe79dfbfb8e3b8637c040a9ae204f222d076b
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2025-01-28 22:55:23 -0500

    PWA so far

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
@@ -12,31 +12,22 @@ export default defineConfig({
         "name": "Create React App Sample",
         "icons": [
           {
-            "src": "/favicon.ico",
-            "sizes": "48x48",
-            "type": "image/x-icon",
-            "purpose": "maskable"
+            "src": "./favicon.ico",
+            "sizes": "64x64 32x32 24x24 16x16",
+            "type": "image/x-icon"
           },
           {
-            "src": "/meet-app-144.png",
+            "src": "./logo192.png",
             "type": "image/png",
-            "sizes": "144x144",
-            "purpose": "any"
+            "sizes": "192x192"
           },
           {
-            "src": "/meet-app-192.png",
+            "src": "./logo512.png",
             "type": "image/png",
-            "sizes": "192x192",
-            "purpose": "maskable"
-          },
-          {
-            "src": "/meet-app-512.png",
-            "type": "image/png",
-            "sizes": "512x512",
-            "purpose": "maskable"
+            "sizes": "512x512"
           }
         ],
-        "start_url": "/",
+        "start_url": ".",
         "display": "standalone",
         "theme_color": "#000000",
         "background_color": "#ffffff"
@@ -59,5 +50,11 @@ export default defineConfig({
         ],
       },
     })
-  ]
+  ],
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js']
+    }
+  }
 });
+
